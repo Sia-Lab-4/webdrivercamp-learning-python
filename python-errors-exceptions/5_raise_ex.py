42b8ca76b869c1d88fe6d77472c3b56a2dbdc601
Sia.Lab <180635855+Sia-Lab-4@users.noreply.github.com>
2024-09-26 05:34:03 -0400

    Week 3 Python 3.2 full

---
+++ python-errors-exceptions/5_raise_ex.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def raise_ex():
+    raise TypeError("Type Error")
+if __name__ == "__main__":
+    try:
+        raise_ex()
+    except TypeError as te:
+        print("Exception raised")
