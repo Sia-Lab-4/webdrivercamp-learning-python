b4fb67a725f356faa57bb830953f636d04281387
Sia.Lab <180635855+Sia-Lab-4@users.noreply.github.com>
2024-09-27 18:38:04 -0400

    Everything is object

---
+++ everything_is_object.py
@@ -0,0 +1,60 @@
+# 0. print(type(a))
+
+# 1. print(id(a))
+
+# 2. No
+
+# 3. Yes
+
+# 4. Yes
+
+# 5. No
+
+# 6. True
+
+# 7. True 
+
+# 8. True
+
+# 9. True
+
+# 10. True
+
+# 11. False 
+
+# 12. True
+
+# 13. True 
+
+# 14. 1, 2, 3, 4
+
+# 15. 1, 2, 3
+
+# 16. 1
+
+# 17. 1, 2, 3, 4
+
+# 18. 1 ,2, 3
+
+# 19. True
+#     True
+#     True
+#     False
+
+# 20. tuple
+
+# 21. tuple
+
+# 22. No, int
+
+# 23. tuple
+
+# 24. True  
+
+# 25. True/False (depends on interpreter)
+
+# 26. True
+
+# 27. Different
+
+# 28. 139926795932424
--- python_data_structures_continue/13_dict_v_delete.py
+++ python_data_structures_continue/13_dict_v_delete.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-def dict_v_delete(a_dictionary, value):
-    keys_to_delete = []
-    for key in a_dictionary:
-        if a_dictionary[key] == value:
-            keys_to_delete.append(key)
-    for key in keys_to_delete:
-        del a_dictionary[key]
-
-    return a_dictionary
+def dict_v_delete(a_dictionary, value=None):
+   for key, val in a_dictionary.copy().items():
+    if val == value:
+     del a_dictionary[key]
+     print(f"{key}: {val}")
 
 
+
+   return a_dictionary
+
+# dict_ = {"libs": (1,2,3), "x": "Selenium", "lang": ["Java"], "frame": "Selenium"}
+
+# dict_v_delete(dict_)
 
 if __name__=="__main__":
     dict_print = __import__('6_dict_print').dict_print
@@ -20,3 +22,4 @@ if __name__=="__main__":
     new_dict = dict_v_delete(dict_, 'y')
     print(f"{'Updated Dict':.^20}")
     dict_print(new_dict)
+
